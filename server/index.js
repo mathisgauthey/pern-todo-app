a2b0ef64b41228383d52fe6ebe29e561b7206c59
mathisgauthey <mathis.gauthey@gmail.com>
2024-01-15 19:01:20 +0100

    ROUTES: get a todo

--- server/index.js
+++ server/index.js
@@ -38,6 +38,19 @@ app.get("/todos", async (req, res) => {
 
 //get a todo
 
+app.get("/todos/:id", async (req, res) => {
+    try {
+        // console.log(req.params);
+        const { id } = req.params;
+        const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+            id,
+        ]);
+        res.json(todo.rows[0]);
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
 //update a todo
 
 //delete a todo
