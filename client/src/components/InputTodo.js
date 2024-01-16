24b4bde64e69deba95e227864d6569ab1e83dd74
mathisgauthey <mathis.gauthey@gmail.com>
2024-01-16 11:18:22 +0100

    added setDescription

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -11,6 +11,7 @@ const InputTodo = () => {
                     type="text"
                     className="form-control"
                     value={description}
+                    onChange={(e) => setDescription(e.target.value)}
                 />
                 <button className="btn btn-success">Add</button>
             </form>
