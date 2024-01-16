b48207de31d00f19bdda793852ae6c904e090fa9
mathisgauthey <mathis.gauthey@gmail.com>
2024-01-16 13:55:41 +0100

    added submit form to create todo

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -3,10 +3,26 @@ import React, { Fragment, useState } from "react";
 const InputTodo = () => {
     const [description, setDescription] = useState(""); // description is the value, setDescription is the function to update the value, useState("") is the initial value
 
+    const onSubmitForm = async (e) => {
+        e.preventDefault(); // Don't refresh the page
+        try {
+            const body = { description };
+            const response = await fetch("http://localhost:5000/todos", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(body),
+            });
+
+            console.log(response);
+        } catch (error) {
+            console.error(error.message);
+        }
+    };
+
     return (
         <Fragment>
             <h1 className="text-center mt-5">Pern Todo List</h1>
-            <form className="d-flex mt-5">
+            <form className="d-flex mt-5" onSubmit={onSubmitForm}>
                 <input
                     type="text"
                     className="form-control"
