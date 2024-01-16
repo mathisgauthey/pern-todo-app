0f49a2a2eb09a818f902b0df0284fd8feb9648fb
mathisgauthey <mathis.gauthey@gmail.com>
2024-01-16 11:17:25 +0100

    preparing update value

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -1,11 +1,17 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const InputTodo = () => {
+    const [description, setDescription] = useState(""); // description is the value, setDescription is the function to update the value, useState("") is the initial value
+
     return (
         <Fragment>
             <h1 className="text-center mt-5">Pern Todo List</h1>
             <form className="d-flex mt-5">
-                <input type="text" className="form-control" />
+                <input
+                    type="text"
+                    className="form-control"
+                    value={description}
+                />
                 <button className="btn btn-success">Add</button>
             </form>
         </Fragment>
