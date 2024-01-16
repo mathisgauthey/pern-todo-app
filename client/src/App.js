9af34f801e92683a43968a4ce8b3d3334320c8eb
mathisgauthey <mathis.gauthey@gmail.com>
2024-01-16 11:09:28 +0100

    created components

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
 import React, { Fragment } from "react";
 import "./App.css";
+
+// COMPONENTS
+
+import InputTodo from "./components/InputTodo";
+
 function App() {
     return (
         <Fragment>
+            <div className="container">
+                <InputTodo></InputTodo>
+            </div>
         </Fragment>
     );
 }
